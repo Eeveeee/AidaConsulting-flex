4b5486a5e7108f06ac135a45f58fee328c7be4aa
Eeveeee <fx8300gfe@gmail.com>
2020-10-25 07:23:47 +0300

    little fixes

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,19 +2,12 @@ const burgerBtn = document.querySelector('.button-burger')
 const burgerMenu = document.querySelector('.header-nav')
 const navigation = document.querySelectorAll('.nav-selector')
 let currentWidth = document.documentElement.clientWidth
-console.log(navigation)
-
-function widthCheck(currentWidth) {
-  if (currentWidth <= 1000) {
-    return true
-  } else return false
-}
 
 navigation.forEach((selector) => {
   selector.addEventListener('click', (e) => {
     e.preventDefault()
     let equal = document.querySelector(`.${selector.getAttribute('href')}`)
-    equal.scrollIntoView({ behavior: 'smooth' })
+    equal.scrollIntoView({ block: 'center', behavior: 'smooth' })
     if (burgerMenu.classList.contains('active')) {
       burgerBtn.classList.toggle('active')
       burgerMenu.classList.toggle('active')
